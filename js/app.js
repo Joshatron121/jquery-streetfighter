0cf1d6331d354b408969334939f1c76a01cfe32e
Josh Eimer <jeimer@Joshs-MacBook-Pro.local>
2016-08-28 03:52:59 -0500

    finished project v1

--- js/app.js
+++ js/app.js
@@ -8,9 +8,10 @@ $(document).ready(function(){
 			$('.ryu-still').show();
 	})
 	.mousedown(function(){
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show().animate(
+		$('.hadouken').finish().show().animate(
 			{'left': '1020px'},
 			500,
 			function(){
@@ -23,4 +24,23 @@ $(document).ready(function(){
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+	$('body').keydown(function(event){
+		if(event.which == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		};
+	})
+	$('body').keyup(function(event){
+		if(event.which == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		};
+	});
 });
+
+function playHadouken(){
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
